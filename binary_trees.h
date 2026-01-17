c6365a8350cbf697c4df8f60e2c53309d5f6622b
Phinatech <c.ugbo@alustudent.com>
2026-01-17 11:36:14 +0200

    pushing

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
-
 #include <stddef.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  * @n: Integer stored in the node
@@ -10,7 +8,6 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
 typedef struct binary_tree_s
 {
     int n;
@@ -18,9 +15,7 @@ typedef struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 } binary_tree_t;
-
 void binary_tree_print(const binary_tree_t *);
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -34,4 +29,5 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 #endif /* _BINARY_TREES_H_ */
